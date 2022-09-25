9f90a51cdcbbaa0c6519633ef7ca3c35d72f7f1d
Lukasz Bolach <bravo1_lima1@yahoo.com>
2022-09-25 16:24:44 +0200

    initial

---
+++ config.js
@@ -0,0 +1,7 @@
+
+module.exports = {
+    db: 'mongodb+srv://admin:12345@cluster0.9eir9ld.mongodb.net/?retryWrites=true&w=majority',
+    keySession: ['twojklucz'],
+    maxAgeSession: 24 * 60 * 60 * 1000,
+
+}
---
+++ routes/news.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+const News = require('../models/news');
+
+/* GET home page. */
+router.get('/', (req, res) => {
+    const search = req.query.search || '';
+
+    const findNews = News
+        .find({title: new RegExp(search.trim(), 'i')})
+        .sort({created: -1});
+
+    findNews.exec((err, data) => {
+        res.render('news', {title: 'News', data, search});
+
+    });         //********17.53 kurs 9/10 dziala
+
+});
+
+module.exports = router;
